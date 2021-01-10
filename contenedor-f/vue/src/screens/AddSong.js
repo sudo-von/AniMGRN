7bbf49166def473a6c5d1a07b99041085d938534
V̵O̵N̵ <developerjesus@hotmail.com>
2021-01-10 04:27:52 -0600

    feat: :sparkles: Add VueJS folder, VueJS webpack config file, VueJS base project structure.

---
+++ contenedor-f/vue/src/screens/AddSong.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+
+Vue.component('add-song',{
+  template: 
+  `
+    <div>
+        <navbar page="Add song"></navbar>
+        <div class="container">
+            <p></p>
+        </div>
+    </div>
+  `
+})
---
+++ contenedor-f/vue/src/screens/Playlist.js
@@ -0,0 +1,13 @@
+import Vue from 'vue'
+
+Vue.component('playlist',{
+  template: 
+  `
+    <div>
+        <navbar page="Playlist"></navbar>
+        <div class="container">
+            <p></p>
+        </div>
+    </div>
+  `
+})
