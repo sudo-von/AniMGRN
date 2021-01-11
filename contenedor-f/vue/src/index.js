b4a01fa048248ee52a7c3cf20d9d7ede30e8c524
V̵O̵N̵ <developerjesus@hotmail.com>
2021-01-10 21:57:17 -0600

    feat: :sparkles: Updat components, add base template for the screens and the screens are responsive

--- contenedor-f/vue/src/index.js
+++ contenedor-f/vue/src/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Navbar from './components/Navbar'
 import Sidenav from './components/Sidenav'
+import MediaPlayer from './components/MediaPlayer'
 import Playlist from './screens/Playlist'
 import AddSong from './screens/AddSong'
 
