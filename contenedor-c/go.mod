13ff8d8a936bfc72d295bb4ae35e2b0a077ca26d
V̵O̵N̵ <58245568+sudo-von@users.noreply.github.com>
2021-01-11 23:32:41 -0600

    Revert "refactor: :recycle: Add repository pattern to the web scrapper"

--- contenedor-c/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/AniMGRN/contenedor-c
-
-go 1.14
-
-require gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22
--- contenedor-f/vue/src/index.js
+++
@@ -1,8 +0,0 @@
-import Vue from 'vue'
-import Navbar from './components/Navbar'
-import Sidenav from './components/Sidenav'
-import MediaPlayer from './components/MediaPlayer'
-import Playlist from './screens/Playlist'
-import AddSong from './screens/AddSong'
-
-new Vue({ el: '#app'})
